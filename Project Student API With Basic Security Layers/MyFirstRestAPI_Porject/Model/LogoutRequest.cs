407a149af036f0106d5fbd32d0b594c4c83dce68
Mouad <162752950+MProgrammer01@users.noreply.github.com>
2026-04-01 19:16:54 +0100

    added

---
+++ Project Student API With Basic Security Layers/MyFirstRestAPI_Porject/Model/LogoutRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentApi.Model
+{
+    public class LogoutRequest
+    {
+        public string Email { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ Project Student API With Basic Security Layers/MyFirstRestAPI_Porject/Model/RefreshRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentApi.Model
+{
+    public class RefreshRequest
+    {
+        public string RefreshToken { get; set; }
+        public string Email { get; set; }
+    }
+}
